978b3939986238661e2e20c622164e8ed1f661ff
joeface71 <joefacenda@yahoo.com>
2020-02-09 16:07:36 -0500

    End of module 5

--- BlazorMovies/Shared/Entities/Genre.cs
+++ BlazorMovies/Shared/Entities/Genre.cs
@@ -10,5 +10,6 @@ namespace BlazorMovies.Shared.Entities
         public int Id { get; set; }       
         [Required(ErrorMessage = "This field is required")]
         public string Name { get; set; }
+        public List<MoviesGenres> MoviesGenres { get; set; } = new List<MoviesGenres>();
     }
 }
